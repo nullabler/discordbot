442d415fba656f2a65122e1ef2838281063ef0a8
Stepan Barannikov <mail@tmake.ru>
2021-11-18 21:02:14 +0300

    add prod env

--- scripts/system.mk
+++ scripts/system.mk
@@ -1,18 +1,18 @@
 ##@ System:
 
 up: ## start project
-	docker-compose --env-file configs/.env -f deploy/docker-compose.yml up -d
+	docker-compose --env-file configs/.env -f deploy/prod/docker-compose.yml up -d
 
 down: ## stop project
-	docker-compose down
+	docker-compose --env-file configs/.env -f deploy/prod/docker-compose.yml down
 
 watch: ## watch project
-	docker-compose --env-file configs/.env -f deploy/docker-compose.yml up 
+	docker-compose --env-file configs/.env -f deploy/dev/docker-compose.yml up 
 
 state: ## show state
-	docker-compose ps
+	docker-compose --env-file configs/.env -f deploy/prod/docker-compose.yml ps
 
 logs: ## show last 100 lines of logs
-	docker-compose logs --tail=100 $(ARGS)
+	docker-compose --env-file configs/.env -f deploy/prod/docker-compose.yml logs --tail=100 $(ARGS)
 
 
